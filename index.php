97196b3acf997db126e18171907b0415d936604f
Vitaliy Bondarenko <bovile17051992@gmail.com>
2022-11-05 16:30:24 +0200

    Refactor

--- index.php
+++ index.php
@@ -12,7 +12,9 @@ echo <<<HTML
         <h1>Заголовок {$date}</h1>
 HTML;
 
-for($i = 1; $i <= 100 ; $i++){
+$count = 100;
+
+for($i = 1; $i <= $count ; $i++){
     echo "<p>{$i})Текст текст текст</p>";
 }
 
