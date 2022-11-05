01b4dc699fb99d8eb2b5ae501d8d4f1dab8b4966
Vitaliy Bondarenko <bovile17051992@gmail.com>
2022-11-05 16:18:15 +0200

    Start track index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$date = date('Y-m-d H:i:s');
+
+echo <<<HTML
+<!doctype>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Заголовок {$date}</h1>
+HTML;
+
+for($i = 1; $i <= 10 ; $i++){
+    echo "<p>{$i})Текст текст текст</p>";
+}
+
+echo <<<HTML
+    </body>
+</html>
+HTML;
+
